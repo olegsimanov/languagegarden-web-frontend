15473f44c2ddadb27cf8dd257771f896a18b2d4d
Oleg Simanov <olegsimanov@gmail.com>
2021-08-06 23:11:20 +0400

    code formatting, adding comments

--- scripts/setup/install.sh
+++ scripts/setup/install.sh
@@ -6,5 +6,5 @@ source "$SCRIPT_DIR/set_vars.sh"
 set -euo pipefail
 
 $SCRIPT_DIR/update_system_requirements.sh             # installs brew packages
-$SCRIPT_DIR/setup_venv.sh                             # installs nodeenv (required by the following script)
-$SCRIPT_DIR/update_frontend_requirements.sh           #
+$SCRIPT_DIR/setup_venv.sh                             # installs nodeenv by using pip! (required by the following script)
+$SCRIPT_DIR/update_frontend_requirements.sh           # installs npm global and local requirements, and installs bower
