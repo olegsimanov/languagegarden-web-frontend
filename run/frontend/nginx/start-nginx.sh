311b1e4a118fdf0ff731bb1e125d42ad6ba14e19
Oleg Simanov <olegsimanov@gmail.com>
2021-10-24 12:25:09 +0400

    player.html doesn't exist any more

--- run/frontend/nginx/start-nginx.sh
+++ run/frontend/nginx/start-nginx.sh
@@ -8,6 +8,5 @@ docker run --rm \
 -v "${SCRIPT_DIR}"/default.conf:/etc/nginx/conf.d/default.conf:ro \
 -v "${SCRIPT_DIR}"/nginx.conf:/etc/nginx/nginx.conf:ro  \
 -v "${SCRIPT_DIR}"/../../../components/build/:/var/www/html/static/ \
--v "${SCRIPT_DIR}"/../player.html/:/var/www/html/player.html/ \
 -v "${SCRIPT_DIR}"/../editor.html/:/var/www/html/editor.html/ \
 --name languagegarden-frontend-modified -p 5442:80 scr.saal.ai/nginx:1.17.1
