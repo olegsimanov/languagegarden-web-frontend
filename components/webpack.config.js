c2508fbe532a05a88428716048a3df6f8813c83c
Oleg Simanov <olegsimanov@gmail.com>
2021-10-24 12:45:29 +0400

    removing unused code

--- components/webpack.config.js
+++ components/webpack.config.js
@@ -1,29 +1,13 @@
 (function () {
     'use strict';
 
-    var webpackConfig = require('./webpack-instance-config');
+    var webpack             = require('webpack');
+    var path                = require('path');
+    var ExtractTextPlugin   = require('extract-text-webpack-plugin');
 
-    var webpack = require('webpack');
-    var path = require('path');
-    var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-    var srcScriptsDir = path.join(__dirname, 'app', 'scripts');
-    var targetDir = path.join(__dirname, 'build');
-    var bowerComponentsDir = path.join(__dirname, 'bower_components');
-
-    var buildType;
-    var developmentVersion;
-    var sentryPublicDSN = null;
-
-    buildType = process.env.BUILD_TYPE || 'development';
-    developmentVersion = buildType === 'development';
-    if (webpackConfig.sentry && webpackConfig.sentry.publicDSN) {
-        sentryPublicDSN = webpackConfig.sentry.publicDSN;
-    }
-
-    if (!developmentVersion) {
-        targetDir = path.join(__dirname, 'dist');
-    }
+    var srcScriptsDir       = path.join(__dirname, 'app', 'scripts');
+    var targetDir           = path.join(__dirname, 'build');                // was 'dist' in case of non-development environment (please check history)
+    var bowerComponentsDir  = path.join(__dirname, 'bower_components');
 
     module.exports = {
         context: path.join(__dirname, 'app'),
@@ -31,11 +15,11 @@
             'starter': path.join(srcScriptsDir, 'languagegarden', 'starter.coffee')
         },
         output: {
-            path: targetDir,
-            filename: path.join('js', 'lg-[name].js'),
-            chunkFilename: path.join('js', '[hash]', 'lg-chunk-[id].js'),
-            namedChunkFilename: path.join('js', '[hash]', 'lg-chunk-[name].js'),
-            publicPath: webpackConfig.output.publicPath || ''
+            path:                 targetDir,
+            filename:             path.join('js', 'lg-[name].js'),
+            chunkFilename:        path.join('js', '[hash]', 'lg-chunk-[id].js'),
+            namedChunkFilename:   path.join('js', '[hash]', 'lg-chunk-[name].js'),
+            publicPath:           '/static/'
         },
         module: {
             loaders: [
@@ -63,17 +47,14 @@
         },
         plugins: [
             new webpack.DefinePlugin({
-                __DEV__: developmentVersion,
-                __SENTRY_PUBLIC_DSN__: JSON.stringify(sentryPublicDSN)
+                __DEV__:                'development',
+                __SENTRY_PUBLIC_DSN__:  null
             }),
-            //new webpack.optimize.CommonsChunkPlugin('common.js'),
-            // TODO: use the css/ path when the relative file path problem
-            // (url(...)) will be resolved.
             new ExtractTextPlugin('lg-[name].css'),
             new webpack.ProvidePlugin({
-                _: 'underscore',
-                jQuery: 'jquery',
-                Backbone: 'backbone'
+                _:          'underscore',
+                jQuery:     'jquery',
+                Backbone:   'backbone'
             }),
             new webpack.ResolverPlugin(new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
             )
@@ -81,22 +62,6 @@
         resolve: {
             root: [
                 bowerComponentsDir,
-
-                path.join(bowerComponentsDir, 'bootstrap-modal', 'js'),
-                path.join(bowerComponentsDir, 'bootstrap', 'js'),
-                path.join(bowerComponentsDir, 'brandimint-xediatable-bootstrap', 'js'),
-
-                path.join(bowerComponentsDir, 'backbone.localstorage'),
-                path.join(bowerComponentsDir, 'backbone.bootstrap-modal', 'src'),
-
-                path.join(bowerComponentsDir, 'jquery.cookie'),
-                path.join(bowerComponentsDir, 'jquery-simplecolorpicker'),
-                path.join(bowerComponentsDir, 'jquery-ui', 'ui'),
-
-                path.join(bowerComponentsDir, 'jscrollpane', 'script'),
-
-                path.join(bowerComponentsDir, 'swfobject-amd'),               // could be removed
-
             ],
             extensions: ['', '.js', '.coffee', '.json']
         }
